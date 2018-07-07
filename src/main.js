25837a93c4e75b708f5936fc5dc3767e17349983
xuxuelili <2211903076@qq.com>
2018-07-07 10:21:01 +0800

    初始化-配置路由

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
---
+++ src/routes/router.js
@@ -0,0 +1,12 @@
+//导入vue-router
+import VueRouter from 'vue-router'
+
+//创建路由对象, 配置路由规则
+const router = new VueRouter({
+    routes: [
+        
+    ]
+})
+
+//导出模块
+export default router
